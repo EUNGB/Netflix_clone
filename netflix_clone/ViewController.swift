a748130213124bb746ec14e72bf583831ca9ab68
LeeEunBi <eblee@weedscomm.com>
2021-06-04 14:01:13 +0900

    Initial Commit

---
+++ netflix_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix_clone
+//
+//  Created by Weeds on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
